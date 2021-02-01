dbbcf880785ac1e9db170aeddf4cd9defa77207a
Doryan <doryan.leconte@gmail.com>
2021-02-01 15:47:31 +0100

    Ajout Albums

--- tp1/lib/main.dart
+++ tp1/lib/main.dart
@@ -11,103 +11,174 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.blueGrey,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: NavBar(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class AlbumWidget extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _AlbumWidgetState createState() => _AlbumWidgetState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _AlbumWidgetState extends State<AlbumWidget> {
+  final TextStyle _biggerFont = const TextStyle(fontSize: 18);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _buildListView(),
+    );
+  }
 
-  void _incrementCounter() {
+  Widget _buildListView() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16),
+      itemCount:
+          albums.length * 2, //a cause du diviseur qui prends tous les impairs
+      itemBuilder: (BuildContext _context, int i) {
+        print(albums.length);
+        if (i.isOdd) {
+          return Divider();
+        }
+        final int index = i ~/ 2;
+
+        return _buildRow(albums[index]);
+      },
+    );
+  }
+
+  Widget _buildRow(AlbumModel album) {
+    return ListTile(
+      title: Text(
+        album.nom,
+        style: _biggerFont,
+      ),
+      leading: Image.network(album.urlPhoto),
+      subtitle: Text("par " +
+          album.artiste +
+          " le " +
+          album.date.day.toString() +
+          "/" +
+          album.date.month.toString() +
+          "/" +
+          album.date.year.toString()),
+      /* trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),*/
+      /*    onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },*/
+    );
+  }
+}
+
+/// This is the stateful widget that the main application instantiates.
+class NavBar extends StatefulWidget {
+  NavBar({Key key}) : super(key: key);
+
+  @override
+  _NavBarState createState() => _NavBarState();
+}
+
+/// This is the private State class that goes with MyStatefulWidget.
+class _NavBarState extends State<NavBar> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Favoris',
+      style: optionStyle,
+    ),
+    AlbumWidget(),
+    Text(
+      'Artistes',
+      style: optionStyle,
+    ),
+    Text(
+      'Infos',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _selectedIndex = index;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('Mediathèque'),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+        child: _widgetOptions.elementAt(_selectedIndex),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.album),
+            label: 'Albums',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.mic),
+            label: 'Artistes',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.info),
+            label: 'Infos',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.red[800],
+        unselectedItemColor: Colors.grey,
+        onTap: _onItemTapped,
+      ),
     );
   }
 }
+
+class AlbumModel {
+  String nom;
+  String urlPhoto;
+  DateTime date;
+  String artiste;
+
+  AlbumModel({this.nom, this.date, this.urlPhoto, this.artiste});
+}
+
+final albums = [
+  AlbumModel(
+      nom: "Origin of Symmetry",
+      artiste: "Muse",
+      date: new DateTime(2001, 07, 17),
+      urlPhoto:
+          "https://images-na.ssl-images-amazon.com/images/I/81avaagb6-L._SL1417_.jpg"),
+  AlbumModel(
+      nom: "The Black Parade",
+      artiste: "My Chemical Romance",
+      date: new DateTime(2006, 10, 23),
+      urlPhoto:
+          "https://media.senscritique.com/media/000004867344/source_big/The_Black_Parade.jpg"),
+  AlbumModel(
+      nom: "Neotheater",
+      date: new DateTime(2019, 04, 26),
+      urlPhoto:
+          "https://images-na.ssl-images-amazon.com/images/I/81zKpNSNK1L._SL1500_.jpg",
+      artiste: "AJR")
+];
