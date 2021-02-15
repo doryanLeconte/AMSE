6d222255085d4abcb1016aa2e7f07ea4469a10d5
Doryan <doryan.leconte@gmail.com>
2021-02-15 14:41:21 +0100

    TP2: Exo1 Fini

---
+++ tp2/taquin/android/app/src/main/kotlin/com/imtld/doryanfatima/taquin/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.imtld.doryanfatima.taquin
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ tp2/taquin/lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+import 'exo1/exo1.dart';
+
+void main() {
+  runApp(Exo1());
+}
